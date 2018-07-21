d321fb21427ba83a9514b4025d367751ad7f5c13
knrt10 <tripathi.kautilya@gmail.com>
2018-07-21 18:31:45 +0530

    added runner.conf file :fire:

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Fuck this shit server you shit.</h1>")
 }
 
-func main(){
+func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
 }

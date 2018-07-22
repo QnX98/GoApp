cd8018f10eebccd15623e2dc9f67b2c072cea356
knrt10 <tripathi.kautilya@gmail.com>
2018-07-22 13:11:55 +0530

    Adding status code to 404 pages

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Fuck this shit server you shit.</h1>")
+
+	// setting headers
+	w.Header().Set("Content-Type", "text/html")
+	// this gets the URL path
+	path := r.URL.Path
+	if path == "/" {
+		fmt.Fprint(w, "<h1>Fuck this shit server you at homepage niggaa.</h1>")
+	} else if path == "/contact" {
+		fmt.Fprint(w, "<h1>Contact bitch, Fuck this shit server you at contact page niggaa.</h1>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>404 bitch, Fuck this shit server you at page not found niggaa.</h1>")
+	}
 }
 
 func main() {

f66a4dc7a3d3f5fda0a9bd9250c6ac2ea8c3f7f5
knrt10 <tripathi.kautilya@gmail.com>
2018-07-23 21:10:23 +0530

    added 404 page

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 )
 
 func contactPageHandler(w http.ResponseWriter, r *http.Request) {
+
 	// setting headers
 	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Contact bitch, Fuck this shit server you at contact page niggaa.</h1>")
@@ -18,9 +19,16 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Fuck this shit server you at homepage niggaa.</h1>")
 }
 
+func notFoundPageHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404 bitch, Fuck this shit server you at page not found niggaa.</h1>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", homePageHandler)
 	r.HandleFunc("/contact", contactPageHandler)
+	r.NotFoundHandler = http.HandlerFunc(notFoundPageHandler)
 	http.ListenAndServe(":3000", r)
 }

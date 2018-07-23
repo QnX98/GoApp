6a4541037fc8d2ca368387f00adbd5671858497f
knrt10 <tripathi.kautilya@gmail.com>
2018-07-23 20:25:36 +0530

    Added gorilla/mux router

--- main.go
+++ main.go
@@ -2,26 +2,25 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
-
+func contactPageHandler(w http.ResponseWriter, r *http.Request) {
 	// setting headers
 	w.Header().Set("Content-Type", "text/html")
-	// this gets the URL path
-	path := r.URL.Path
-	if path == "/" {
-		fmt.Fprint(w, "<h1>Fuck this shit server you at homepage niggaa.</h1>")
-	} else if path == "/contact" {
-		fmt.Fprint(w, "<h1>Contact bitch, Fuck this shit server you at contact page niggaa.</h1>")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>404 bitch, Fuck this shit server you at page not found niggaa.</h1>")
-	}
+	fmt.Fprint(w, "<h1>Contact bitch, Fuck this shit server you at contact page niggaa.</h1>")
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	// setting headers
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Fuck this shit server you at homepage niggaa.</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", homePageHandler)
+	r.HandleFunc("/contact", contactPageHandler)
+	http.ListenAndServe(":3000", r)
 }
